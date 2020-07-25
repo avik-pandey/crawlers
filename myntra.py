096f8e70ff7fdb6e75903568e527635e19b5f25a
Avik Pandey <avikpandey1@gmail.com>
2020-07-25 18:55:41 +0530

    ok

--- myntra.py
+++ myntra.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[22]:
+# In[9]:
 
 
 import selenium
 from selenium.webdriver import ActionChains
 
 
-# In[23]:
+# In[10]:
 
 
 from selenium import webdriver
@@ -36,7 +36,7 @@ myntraProducts = db.myntra
 print(myntraProducts)
 
 
-# In[24]:
+# In[11]:
 
 
 options = Options()
@@ -65,15 +65,17 @@ def patching_get(driver, url):
     return driver
 
 
-# In[25]:
+# In[12]:
 
 
 allCategories = []
 def getCategories(driver,base_url = "https://www.myntra.com/"):
-    driver = patching_get(driver,base_url)
-    menPath = driver.find_element_by_xpath('//*[@id="desktop-header-cnt"]/div[2]/nav/div/div[1]/div/a')
-    womenPath = driver.find_element_by_xpath('//*[@id="desktop-header-cnt"]/div[2]/nav/div/div[2]/div/a')
-    kidsPath = driver.find_element_by_xpath('//*[@id="desktop-header-cnt"]/div[2]/nav/div/div[3]/div/a')
+    try:
+        
+        driver = patching_get(driver,base_url)
+        menPath = driver.find_element_by_xpath('//*[@id="desktop-header-cnt"]/div[2]/nav/div/div[1]/div/a')
+        womenPath = driver.find_element_by_xpath('//*[@id="desktop-header-cnt"]/div[2]/nav/div/div[2]/div/a')
+        kidsPath = driver.find_element_by_xpath('//*[@id="desktop-header-cnt"]/div[2]/nav/div/div[3]/div/a')
 #     paths = []
 #     paths.append(menPath)
 #     paths.append(womenPath)
@@ -110,91 +112,95 @@ def getCategories(driver,base_url = "https://www.myntra.com/"):
 #             catArr.append(catName)
 #             catArr.append(links)
 #         allCategories.append(catArr)
-    majCategories = driver.find_elements_by_class_name('desktop-categoryContainer')
-    print(len(majCategories))
-    cnt = 0
-    for el in majCategories:
-        cnt = cnt+1
-        if(cnt>2):
-            break
-        majCatName = ""
-        if(cnt == 1):
-            majCatName = menPath.text
-        elif(cnt == 2):
-            majCatName = womenPath.text
-        else:
-            majCatName = kidsPath.text
-        lowCategories = el.find_elements_by_tag_name('a')
-        tot = []
-        allLinks = []
-        for lowCat in lowCategories:
-            singleLink = []
-            name = lowCat.get_attribute('textContent')
+        majCategories = driver.find_elements_by_class_name('desktop-categoryContainer')
+        print(len(majCategories))
+        cnt = 0
+        for el in majCategories:
+            cnt = cnt+1
+            if(cnt>2):
+                break
+            majCatName = ""
+            if(cnt == 1):
+                majCatName = menPath.text
+            elif(cnt == 2):
+                majCatName = womenPath.text
+            else:
+                majCatName = kidsPath.text
+            lowCategories = el.find_elements_by_tag_name('a')
+            tot = []
+            allLinks = []
+            for lowCat in lowCategories:
+                singleLink = []
+                name = lowCat.get_attribute('textContent')
             
-            link = lowCat.get_attribute('href')
-            singleLink.append(name)
-            singleLink.append(link)
-            allLinks.append(singleLink)
-        tot.append(majCatName)
-        tot.append(allLinks)
-        allCategories.append(tot)
-            
+                link = lowCat.get_attribute('href')
+                singleLink.append(name)
+                singleLink.append(link)
+                allLinks.append(singleLink)
+            tot.append(majCatName)
+            tot.append(allLinks)
+            allCategories.append(tot)
+    except Exception as e:
+        print(e)
+        raise(e)
        
     return allCategories   
         
     
 
 
-# In[26]:
+# In[13]:
 
 
 getCategories(driver)
 
 
-# In[27]:
+# In[14]:
 
 
 allProducts = []
 
 def getAllProducts(driver):
-    for el in allCategories:
-        majCatName = el[0]
-        print(majCatName)
-        for e in el[1]:
-            secCatName = e[0]
-            print(secCatName)
-            driver = patching_get(driver,e[1])
-            try:
-                
-                img_links = driver.find_elements_by_class_name('product-sliderContainer')
-                time.sleep(3)
-            except:
-                pass
-            print(len(img_links))
-            myLength = len(WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.TAG_NAME,"img"))))
-#             images = driver.find_elements_by_tag_name('img')
-            images = []
-            while True:
-            
-                driver.execute_script("window.scrollBy(0,800)", "")
+    try:
+        
+        for el in allCategories:
+            majCatName = el[0]
+            print(majCatName)
+            for e in el[1]:
+                secCatName = e[0]
+                print(secCatName)
+                driver = patching_get(driver,e[1])
                 try:
-                    WebDriverWait(driver, 20).until(lambda driver: len(driver.find_elements_by_tag_name("img")) > myLength)
-                    images = driver.find_elements_by_tag_name("img")
-                    myLength = len(images)
-                except TimeoutException:
-                    break
-            print(len(images))
-            all_img_links = []
-            for ele in images:
-                imgLink = ele.get_attribute('src')
-                time.sleep(0.1)
-                imgLink = ele.get_attribute('src')
-                all_img_links.append(imgLink)
                 
-            print(len(all_img_links))
+                    img_links = driver.find_elements_by_class_name('product-sliderContainer')
+                    time.sleep(3)
+                except:
+                    pass
+                print(len(img_links))
+                myLength = len(WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.TAG_NAME,"img"))))
+#             images = driver.find_elements_by_tag_name('img')
+                images = []
+                while True:
             
-            trendingInfo = []
-            for image in img_links:
+                    driver.execute_script("window.scrollBy(0,800)", "")
+                    try:
+                        WebDriverWait(driver, 20).until(lambda driver: len(driver.find_elements_by_tag_name("img")) > myLength)
+                        images = driver.find_elements_by_tag_name("img")
+                        myLength = len(images)
+                    except TimeoutException:
+                        break
+                print(len(images))
+                all_img_links = []
+                for ele in images:
+                    imgLink = ele.get_attribute('src')
+                    time.sleep(0.1)
+                    imgLink = ele.get_attribute('src')
+                    all_img_links.append(imgLink)
+                
+                print(len(all_img_links))
+            
+                trendingInfo = []
+                for image in img_links:
                 #imgLink = image.find_element_by_class_name('img-responsive').get_attribute('src')
 #                 try:
 #                     imgLink = WebDriverWait(image, 20).until(EC.presence_of_element_located((By.TAG_NAME, "img")))
@@ -205,45 +211,48 @@ def getAllProducts(driver):
                 
 #                 finImg = imgLink.get_attribute('src')
 #                 print(finImg)
-                temp = False
-                try:
-                    tmp1=image.find_element_by_class_name("xcelerator-plpXceleratorImageTag")
+                    temp = False
+                    try:
+                        tmp1=image.find_element_by_class_name("xcelerator-plpXceleratorImageTag")
 #                     time.sleep(0.1)
 #                     tmp1 = image.find_element_by_class_name("xcelerator-plpXceleratorImageTag")
-                    temp = True
-                except NoSuchElementException:
+                        temp = True
+                    except NoSuchElementException:
 #                     print('amaa')
-                    temp = False
+                         temp = False
                     
 #                 all_img_links.append(finImg)
-                trendingInfo.append(temp)
+                    trendingInfo.append(temp)
              
-            productInfo = driver.find_elements_by_class_name('product-productMetaInfo')
-            companyName = []
-            moreProductInfo = []
+                productInfo = driver.find_elements_by_class_name('product-productMetaInfo')
+                companyName = []
+                moreProductInfo = []
             
-            for prod in productInfo:
-                brandName = prod.find_element_by_class_name('product-brand').text
-                productName = prod.find_element_by_class_name('product-product').text
-                companyName.append(brandName)
-                moreProductInfo.append(productName)
+                for prod in productInfo:
+                    brandName = prod.find_element_by_class_name('product-brand').text
+                    productName = prod.find_element_by_class_name('product-product').text
+                    companyName.append(brandName)
+                    moreProductInfo.append(productName)
             
-            size = len(companyName)
+                size = len(companyName)
             
-            for i in range(size):
-                if len(all_img_links) == 0:
-                    break
-                fin = {
+                for i in range(size):
+                    if len(all_img_links) == 0:
+                        break
+                    fin = {
                     'Major-Category':majCatName,
                     'Sub-Category':secCatName,
                     'Image':all_img_links[i],
                     'Trending':trendingInfo[i],
                     'Brand':companyName[i],
                     'Product-info':moreProductInfo[i]
-                }
-                print(fin)
-                allProducts.append(fin)
-                
+                   }
+                    print(fin)
+                    allProducts.append(fin)
+    
+    except Exception as e:
+        print(e)
+        raise(e)
     driver.quit()
     return allProducts
             
@@ -251,13 +260,13 @@ def getAllProducts(driver):
     
 
 
-# In[28]:
+# In[15]:
 
 
 getAllProducts(driver)
 
 
-# In[39]:
+# In[16]:
 
 
 print(len(allProducts))
@@ -268,7 +277,7 @@ myntraProducts = db.myntra
 print(myntraProducts)
 myntraProducts.remove()
 # result = myntraProducts.insert_many(allProducts)
-result = myntraProducts.insert_many([{'i': i} for i in range(6244)]).inserted_ids
+result = myntraProducts.insert_many([{'i': i} for i in range(len(allProducts))]).inserted_ids
 print(result)
 # for object_id in result.inserted_ids:
 #     print(str(object_id))
--- vogue_crawler.py
+++ vogue_crawler.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[12]:
+# In[16]:
 
 
 import selenium
 from selenium.webdriver import ActionChains
 
 
-# In[13]:
+# In[17]:
 
 
 from selenium import webdriver
@@ -29,14 +29,14 @@ from pymongo import MongoClient
 import urllib 
 import dns
 #client = MongoClient('mongodb+srv://user:' + urllib.parse.quote('Avik@838') +' @cluster0.pe7lw.mongodb.net/intelFashion?retryWrites=true&w=majority')
-client = MongoClient('mongodb+srv://user:'+urllib.parse.quote('Avik@838')+'@cluster0.pe7lw.mongodb.net/intelFashion?retryWrites=true&w=majority')
-print(client)
-db = client['intelFashion']
-magazineImg = db.magazine
-print(magazineImg)
+# client = MongoClient('mongodb+srv://user:'+urllib.parse.quote('Avik@838')+'@cluster0.pe7lw.mongodb.net/intelFashion?retryWrites=true&w=majority')
+# print(client)
+# db = client['intelFashion']
+# magazineImg = db.magazine
+# print(magazineImg)
 
 
-# In[14]:
+# In[18]:
 
 
 options = Options()
@@ -65,101 +65,118 @@ def patching_get(driver, url):
     return driver
 
 
-# In[15]:
+# In[19]:
 
 
 images = []
 def getImages(driver,base_url = "https://www.vogue.in"):
-    driver = patching_get(driver,base_url)
-    print("yeha")
-    time.sleep(1);
-    ele = driver.find_element_by_xpath('//*[@id="app-root"]/section/div[2]/div[2]/div/div/div[2]/div/div/div/div/div[1]/div/div/a[1]')
-    link = driver.find_element_by_xpath('//*[@id="app-root"]/section/div[2]/div[2]/div/div/div[2]/div/div/div/div/div[1]/div/div/a[1]').text
-    lee = ele.get_attribute('href')
-    print(link,"okk")
-    print(lee,"dkjak")
     try:
-        ele.click()
-        print('clcike')
-        time.sleep(4)
         
-        print('xuasx')
+        driver = patching_get(driver,base_url)
+        print("yeha")
+        time.sleep(1);
+        ele = driver.find_element_by_xpath('//*[@id="SlimNavContent"]/div[2]/div/ul/li[1]/a')
+        link = driver.find_element_by_xpath('//*[@id="SlimNavContent"]/div[2]/div/ul/li[1]/a').text
+        lee = ele.get_attribute('href')
+        print(link,"okk")
+        print(lee,"dkjak")
+        try:
+            ele.click()
+            print('clcike')
+            time.sleep(4)
         
-    except:
-        pass
+            print('xuasx')
+        
+        except:
+            pass
    
-    base_url = lee
-    print(base_url,"aska")
-    driver = patching_get(driver,base_url)
+        base_url = lee
+        print(base_url,"aska")
+        driver = patching_get(driver,base_url)
     
 #     ele2 = driver.find_element_by_xpath('//*[@id="app-root"]/section/main/div/div/div[2]/div[2]/div/div[2]/div/div/div/div/a[3]')     
-    try:
-        print('ehy')
-        url = '/fashion/fashion-trends'
-        driver.find_element_by_xpath('//a[@href="'+url+'"]').click()
-    except:
-        pass
-    new_url = base_url + '/fashion-trends'
-    driver = patching_get(driver,new_url)
+        try:
+            print('ehy')
+            url = '/fashion/fashion-trends'
+            driver.find_element_by_xpath('//a[@href="'+url+'"]').click()
+        except:
+            pass
+        new_url = base_url + '/fashion-trends'
+        driver = patching_get(driver,new_url)
     
     
-    condition = True
-    cnt = 0
-    while condition:
-        cnt = cnt + 1
-        myLength = len(WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.TAG_NAME,"img"))))
-        imgLinks = []
-        while True:
-            driver.execute_script("window.scrollBy(0,800)", "")
-            try:
-                WebDriverWait(driver, 20).until(lambda driver: len(driver.find_elements_by_tag_name("img")) > myLength)
-                img_links = driver.find_elements_by_tag_name("img")
-                myLength = len(img_links)
-            except TimeoutException:
-                break
-        print(len(img_links))
+        condition = True
+        cnt = 0
+        while condition:
+            cnt = cnt + 1
+            myLength = len(WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.TAG_NAME,"img"))))
+            imgLinks = []
+            while True:
+                driver.execute_script("window.scrollBy(0,1200)", "")
+                try:
+                    WebDriverWait(driver, 20).until(lambda driver: len(driver.find_elements_by_tag_name("img")) > myLength)
+                    img_links = driver.find_elements_by_tag_name("img")
+                    myLength = len(img_links)
+                except TimeoutException:
+                    break
+            print(len(img_links))
         
-        for el in img_links:
-            name = el.get_attribute("src")
-            time.sleep(0.1)
-            name = el.get_attribute("src")
-            fin = {
+            for el in img_links:
+                name = el.get_attribute("src")
+                time.sleep(0.1)
+                name = el.get_attribute("src")
+                fin = {
                 'img_link':name
-            }
-            images.append(fin)
+                }
+                print(fin)
+                images.append(fin)
         
-        nextBtn = driver.find_elements_by_class_name('bmYSpE')
-        size = len(nextBtn)
-        print(size)
-        if size == 1 and cnt == 1:
-            Btn = nextBtn[0]
-            Btn.click()
-        elif size == 2 :
-            Btn = nextBtn[1]
-            Btn.click()
+            nextBtn = driver.find_elements_by_class_name('bmYSpE')
+            size = len(nextBtn)
+            print(size)
+            if size == 1 and cnt == 1:
+                Btn = nextBtn[0]
+                Btn.click()
+            elif size == 2 :
+                Btn = nextBtn[1]
+                Btn.click()
             
-        else:
-            condition = False
-        
+            else:
+                condition = False
+    except Exception as e: 
+        print(e)
+        raise(e)
         
     driver.quit()    
     return images
     
 
 
-# In[16]:
+# In[20]:
 
 
 getImages(driver)
 
 
-# In[17]:
+# In[21]:
 
 
 # arr = [{ 'img_link':'faka'}]
+client = MongoClient('mongodb+srv://user:'+urllib.parse.quote('Avik@838')+'@cluster0.pe7lw.mongodb.net/intelFashion?retryWrites=true&w=majority')
+print(client)
+db = client['intelFashion']
+magazineImg = db.magazine
+print(magazineImg)
+magazineImg.remove()
 result = magazineImg.insert_many(images)
 for object_id in result.inserted_ids:
     print(str(object_id))
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
