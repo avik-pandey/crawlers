6b3cbdc98fdb35fb3514d1264f8913b7f46feacf
Avik Pandey <avikpandey1@gmail.com>
2020-07-26 02:55:52 +0530

    ok

---
+++ main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os                                                                       
+from multiprocessing import Pool 
+import defs
+
+
+# In[2]:
+
+
+processes = ('myntra.py', 'vogue_crawler.py') 
+# def run_process(process):                                                             
+#     os.system('python {}'.format(process)) 
+
+    
+
+
+# In[3]:
+
+
+pool = Pool(processes=2)                                                        
+pool.map(defs.run_process, processes) 
+
+
+# In[ ]:
+
+
+
+
