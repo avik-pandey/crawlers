f1cf1de84664c61b68d3c8b4b5e71dddfe2f12ab
Avik Pandey <avikpandey1@gmail.com>
2020-07-26 20:06:00 +0530

    ok

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import defs
 # In[2]:
 
 
-processes = ('myntra.py', 'vogue_crawler.py') 
+processes = ('myntra.py', 'vogue_crawler.py','flipkart.py') 
 # def run_process(process):                                                             
 #     os.system('python {}'.format(process)) 
 
@@ -22,7 +22,7 @@ processes = ('myntra.py', 'vogue_crawler.py')
 # In[3]:
 
 
-pool = Pool(processes=2)                                                        
+pool = Pool(processes=3)                                                        
 pool.map(defs.run_process, processes) 
 
 
